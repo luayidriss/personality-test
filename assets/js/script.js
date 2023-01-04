82e61b64d47c194b20e77c6638551f07d7935dec
luayidriss <idrissluay@gmail.com>
2023-01-04 21:32:59 +0000

    Add Variables, Run Game Function and Constants

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,63 @@
-/* Radio Buttons */
-document.addEventListener("DOMContentLoaded", function() {
-    for ( i=0; i<=10; i++ )
-    {
-        let questionNumber = i;
-        return questions=`'input[name="question-`,${questionNumber},`"]:checked`;
-        console.log(questions);
-    }
+// Questions, Answers and Values Variables
+
+let prompts = ['I see myself as someone who is reserved', 
+'I see myself as someone who is generally trusting',
+'I see myself as someone who tends to be lazy',
+'I see myself as someone who is relaxed and handles stress well',
+'I see myself as someone who is outgoing and sociable',
+'I see myself as someone who has few artistic qualities',
+'I see myself as someone who tends to find fault with others',
+'I see myself as someone who does a thorough job',
+'I see myself as someone who gets nervous easily',
+'I see myself as someone who has an active imagination']
+
+let answerValues ={
+    prompt1: [5,4,3,2,1],
+    prompt2: [1,2,3,4,5],
+    prompt3: [5,4,3,2,1],
+    prompt4: [5,4,3,2,1],
+    prompt5: [5,4,3,2,1],
+    prompt6: [1,2,3,4,5],
+    prompt7: [5,4,3,2,1],
+    prompt8: [1,2,3,4,5],
+    prompt9: [1,2,3,4,5],
+    prompt10: [1,2,3,4,5]
 }
-
-
-function checkButton() {    
-    var getSelectedValue = document.querySelector(   
-        'input[name="question-1"]:checked');   
         
-    if(getSelectedValue != null) {   
-           
-    }   
-    else {   
-        alert("You have not responded to all of the statements");   
-    }   
-}    
-
-function calculateExtraversion(){
-    let extra1 = document.querySelector('input[name="question-1"]:checked').value;
-    let extra2 = document.querySelector('input[name="question-6"]:checked').value;
-    let extrascore = calculateExtraversion();
-    return [extra1 + extra2, "extraversion"];
-    console.log(extrascore)
+// Variables
+const startButton = document.getElementById("start-btn")
+const testArea = document.getElementById("test-area")
+const moveButton = document.getElementsByClassName("move-btn")
+const promptElement =document.getElementById("prompts")
+var optionElement = []
+for (let i=1; i<6;i++){
+    optionElement.push(document.getElementById("option" + i));
 }
+startButton.addEventListener('click', runGame)
+
+function runGame(){
+    startButton.classList.add('hide')
+    testArea.classList.remove('hide')
+    for (let i = 0; i < moveButton.length; i++ ) {
+        moveButton[i].classList.remove('hide');
+      }
+    promptElement.innerHTML =`${prompts[0]}`
+
+    for(let j= 0; j < optionElement.length; j++){
+        optionElement[j].setAttribute("data-value",`${answerValues.prompt1[j]}`)
+    }
+    
+}
+
+function loadNextQuestion(){
+
+}
+
+function loadPreviousQuestion(){
+
+}
+
+function showQuestion(){
+
+}
+
