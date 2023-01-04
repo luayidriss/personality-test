2b6b5d8d962946a6b7e480a1e581f49bf741395d
luayidriss <idrissluay@gmail.com>
2023-01-04 22:04:53 +0000

    Add Question Generation and Next Button Functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,28 +11,42 @@ let prompts = ['I see myself as someone who is reserved',
 'I see myself as someone who gets nervous easily',
 'I see myself as someone who has an active imagination']
 
-let answerValues ={
-    prompt1: [5,4,3,2,1],
-    prompt2: [1,2,3,4,5],
-    prompt3: [5,4,3,2,1],
-    prompt4: [5,4,3,2,1],
-    prompt5: [5,4,3,2,1],
-    prompt6: [1,2,3,4,5],
-    prompt7: [5,4,3,2,1],
-    prompt8: [1,2,3,4,5],
-    prompt9: [1,2,3,4,5],
-    prompt10: [1,2,3,4,5]
-}
+let answerValues =[
+    prompt1= [5,4,3,2,1],
+    prompt2= [1,2,3,4,5],
+    prompt3= [5,4,3,2,1],
+    prompt4= [5,4,3,2,1],
+    prompt5= [5,4,3,2,1],
+    prompt6= [1,2,3,4,5],
+    prompt7= [5,4,3,2,1],
+    prompt8= [1,2,3,4,5],
+    prompt9= [1,2,3,4,5],
+    prompt10= [1,2,3,4,5]
+]
+    
         
 // Variables
 const startButton = document.getElementById("start-btn")
+const nextButton = document.getElementById("next-btn")
 const testArea = document.getElementById("test-area")
 const moveButton = document.getElementsByClassName("move-btn")
 const promptElement =document.getElementById("prompts")
-var optionElement = []
+var optionElements = []
 for (let i=1; i<6;i++){
-    optionElement.push(document.getElementById("option" + i));
+    optionElements.push(document.getElementById("option" + i));
 }
+var counter = 0
+//Load Questions
+
+function generateQuestions(){
+    promptElement.innerHTML =`${prompts[counter]}`
+
+    for(let j= 0; j < optionElements.length; j++){
+        optionElements[j].setAttribute("data-value",`${answerValues[counter][j]}`)
+    }
+}
+
+//Start Game Function
 startButton.addEventListener('click', runGame)
 
 function runGame(){
@@ -41,23 +55,30 @@ function runGame(){
     for (let i = 0; i < moveButton.length; i++ ) {
         moveButton[i].classList.remove('hide');
       }
-    promptElement.innerHTML =`${prompts[0]}`
-
-    for(let j= 0; j < optionElement.length; j++){
-        optionElement[j].setAttribute("data-value",`${answerValues.prompt1[j]}`)
+    generateQuestions()
     }
     
-}
 
+
+// Next Buttion Functionality
+nextButton.addEventListener('click', loadNextQuestion)
 function loadNextQuestion(){
-
+    counter++;
+    if (counter !== 10){
+        generateQuestions()
+    }
+    else {
+        alert("no more questions")
+        showResult()
+    }
 }
 
+//Previous Button Functionality
 function loadPreviousQuestion(){
 
 }
 
-function showQuestion(){
+function showResult(){
 
 }
 
