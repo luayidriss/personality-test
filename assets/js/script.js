5417c9fd8802297e22bbf8ec060dfc763f51d4f3
luayidriss <idrissluay@gmail.com>
2023-01-05 21:23:59 +0000

    Change Calculate, Show Result, Next ad Previous Functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,48 @@
 // Questions, Answers and Scores
 
-let prompts = ['I see myself as someone who is reserved', 
-'I see myself as someone who is generally trusting',
-'I see myself as someone who tends to be lazy',
-'I see myself as someone who is relaxed and handles stress well',
-'I see myself as someone who is outgoing and sociable',
-'I see myself as someone who has few artistic qualities',
-'I see myself as someone who tends to find fault with others',
-'I see myself as someone who does a thorough job',
-'I see myself as someone who gets nervous easily',
-'I see myself as someone who has an active imagination']
-
-let answerValues =[
-    prompt1= [5,4,3,2,1],
-    prompt2= [1,2,3,4,5],
-    prompt3= [5,4,3,2,1],
-    prompt4= [5,4,3,2,1],
-    prompt5= [5,4,3,2,1],
-    prompt6= [1,2,3,4,5],
-    prompt7= [5,4,3,2,1],
-    prompt8= [1,2,3,4,5],
-    prompt9= [1,2,3,4,5],
-    prompt10= [1,2,3,4,5]
+let prompts = [
+'I see myself as someone who has few artistic qualities.',
+'I see myself as someone who tends to be lazy.',
+'I see myself as someone who is generally trusting.',
+'I see myself as someone who is reserved.', 
+'I see myself as someone who is relaxed and handles stress well.',
+'I see myself as someone who has an active imagination.',
+'I see myself as someone who does a thorough job.',
+'I see myself as someone who tends to find fault with others.',
+'I see myself as someone who is outgoing and sociable.',
+'I see myself as someone who gets nervous easily.',
+'I see myself as someone who gets nervous easily.'
 ]
 
-let extroversion= 0;
-let agreeableness = 0;
-let conscientiousness = 0;
-let neuroticism = 0;
-let openness = 0
+let answerValues =[
+prompt1= [5,4,3,2,1],
+prompt2= [5,4,3,2,1],
+prompt3= [1,2,3,4,5],
+prompt4= [5,4,3,2,1],
+prompt5= [5,4,3,2,1],
+prompt6= [1,2,3,4,5],
+prompt7= [1,2,3,4,5],
+prompt8= [5,4,3,2,1],
+prompt9= [1,2,3,4,5],
+prompt10= [1,2,3,4,5],
+prompt11= [0,0,0,0],
+];
 
-let  extroLow= "You are more solitary and reserved, be sure not to force yourself to try being extroverted. Honor your introversion.";
-let extroHigh= "You are outgoing and energetic. You like to be with people.";
-let agreeLow= "You are more analytical and detached. Empathy is not a natural instinct to you, that's okay. Know that you must make more mental effort to put yourself in their shoes.";
-let agreeHigh= "You are friendly and compassionate, understanding others comes easy to you.";
-let consLow= "You are more easy-going and laid back. It does not come naturally for you to be organized, you like to go with the flow.";
-let consHigh= "You are efficient and organized. you want to make sure the people around you respect your desire to have a plan.";
-let neuroLow= "You tend to be more secure and stable in your emotions.";
-let neuroHigh= "You are sensitive and tend to be more nervous. You have to know your triggers–what makes you worry? And your calmers–what helps you calm down? So you can be more in control of your moods.";
-let openLow= "You are consistent and cautious. You can easily be accustomed to habits and you can rely in your area of expertise.";
-let openHigh= "You are inventive and curious,let people in your life know that you love to be challenged. Make sure you have creative outlets to express yourself. You also have to make sure your spouse or partner knows your level of adventure so your needs are met.";
+let userValues= [];
+let userTotals=[];
+
+let personalityDescriptors =[
+   'You are inventive and curious,let people in your life know that you love to be challenged. Make sure you have creative outlets to express yourself. You also have to make sure your spouse or partner knows your level of adventure so your needs are met.',
+'You are consistent and cautious. You can easily be accustomed to habits and you can rely in your area of expertise.',
+'You are efficient and organized. you want to make sure the people around you respect your desire to have a plan.',
+'You are more easy-going and laid back. It does not come naturally for you to be organized, you like to go with the flow.',
+'You are outgoing and energetic. You like to be with people.',
+'You are more solitary and reserved, be sure not to force yourself to try being extroverted. Honor your introversion.',
+'You are friendly and compassionate, understanding others comes easy to you.',
+'You are more analytical and detached. Empathy is not a natural instinct to you, that is okay. Know that you must make more mental effort to put yourself in their shoes.',
+'You are sensitive and tend to be more nervous. You have to know your triggers–what makes you worry? And your calmers–what helps you calm down? So you can be more in control of your moods.',
+'You tend to be more secure and stable in your emotions.',
+]
      
 // Variables
 const startButton = document.getElementById("start-btn")
@@ -47,6 +50,7 @@ const nextButton = document.getElementById("next-btn")
 const prevButton = document.getElementById("prev-btn")
 const finishButton = document.getElementById("finish-btn")
 const testArea = document.getElementById("test-area")
+const radioButtons = document.getElementsByClassName('radio')
 const resultArea = document.getElementById("result-area")
 const restartButton = document.getElementById("reset-btn")
 const moveButton = document.getElementsByClassName("move-btn")
@@ -55,6 +59,7 @@ var optionElements = []
 for (let i=1; i<6;i++){
     optionElements.push(document.getElementById("option" + i));
 }
+const personalityText = document.getElementsByClassName('pdescriptor')
 var counter = 0
 const extroText = document.getElementById("extroversion")
 const agreeText = document.getElementById("agreeableness")
@@ -70,6 +75,11 @@ function generateQuestions(){
     for(let j= 0; j < optionElements.length; j++){
         optionElements[j].setAttribute("data-value",`${answerValues[counter][j]}`)
     }
+    for (let k=0; k < radioButtons.length; k++){
+        if(radioButtons[k].checked)
+        radioButtons[k].checked = false;
+    }
+
 }
 
 //Start Game
@@ -84,109 +94,60 @@ function runGame(){
     generateQuestions()
     }
     
-// Next Button
+// Next and Tally Button
 nextButton.addEventListener('click', loadNextQuestion)
 function loadNextQuestion(){
-        readScore()
-        counter++;
-        if (counter < 10){
-            generateQuestions()
-        }
-        else {
-            nextButton.classList.add('hide');
-            finishButton.classList.remove('hide')
-        } 
-    } 
-// Previous Button
-prevButton.addEventListener('click', loadPreviousQuestion)
-function loadPreviousQuestion(){
-    counter--;
-    removeScore()
-    if (counter < 0){
-        alert('This is the first question')
-    }
-    else {
-        generateQuestions()
-    } 
-}
-
-//Read and Tally Score
-function readScore(){
-    const selectedOption = document.querySelector('input[type="radio"]:checked');
-    if(!selectedOption) {
-        alert('Please select your answer!');
-        return;
-    }
-    else{
-        var answerScore = parseInt(selectedOption.nextElementSibling.getAttribute('data-value'));
-        if(counter == 0 || counter == 5){
-            extroversion += answerScore;}
-        else if(counter == 1 || counter ==  6){
-            agreeableness += answerScore;}
-        else if(counter == 2 || counter ==  7){
-            conscientiousness += answerScore;}
-        else if(counter == 3 || counter ==  8){
-            neuroticism += answerScore;}
-        else if(counter == 4 || counter ==  9){
-            openness += answerScore;}
-        else{
-        }
-}}
-// Remove Score from Previous
-function removeScore(){
+    counter++;
     const selectedOption = document.querySelector('input[type="radio"]:checked');
     var answerScore = parseInt(selectedOption.nextElementSibling.getAttribute('data-value'));
-        if(counter == 0 || counter == 5){
-            extroversion -= answerScore;}
-        else if(counter == 1 || counter ==  6){
-            agreeableness -= answerScore;}
-        else if(counter == 2 || counter ==  7){
-            conscientiousness -= answerScore;}
-        else if(counter == 3 || counter ==  8){
-            neuroticism -= answerScore;}
-        else if(counter == 4 || counter ==  9){
-            openness -= answerScore;}
+    userValues.push(answerScore)
+    if(counter < 10){
+        if(!selectedOption) {
+            alert('Please select your answer!');}
         else{
-            alert("Invalid Entry");
-        }
+            generateQuestions()  
+            }
+    }
+    else{
+        nextButton.classList.add('hide');
+        finishButton.classList.remove('hide')
+    }
 }
+// Previous and Remove Score Button
+prevButton.addEventListener('click', loadPreviousQuestion)
+function loadPreviousQuestion(){
+        counter--;
+        if (counter < 0){
+            alert('This is the first question')
+        }
+        else {
+        generateQuestions()
+        userValues.pop();
+        } 
+    }
 
+
+// Calculate Scores
+function calculateScore(){
+    for(i=0; i<5; i++){
+        userTotals.push(userValues[i]+userValues[i+5])
+    }
+}
 // Finish Button and Show Personality Description
 finishButton.addEventListener('click', showResult)
 function showResult(){
     testArea.classList.add('hide')
     prevButton.classList.add('hide')
     finishButton.classList.add('hide')
+    calculateScore();
     resultArea.classList.remove('hide')
-    if (extroversion > 4){
-        extroText.innerHTML = extroHigh
-    }
-    else {
-        extroText.innerText = extroLow
-    }
-    if (agreeableness > 4){
-        agreeText.innerHTML = agreeHigh
-    }
-    else {
-        agreeText.innerText = agreeLow
-    }
-    if (conscientiousness > 4){
-        consText.innerHTML = consHigh
-    }
-    else {
-        consText.innerText = consLow
-    }
-    if (neuroticism > 4){
-        neuroText.innerHTML = neuroHigh
-    }
-    else {
-        neuroText.innerText = neuroLow
-
-    }if (openness > 4){
-        openText.innerHTML = openHigh
-    }
-    else {
-        openText.innerText = openLow
+    for(i=0; i < personalityText.length; i++){
+        if(userTotals[i]>=5){
+            personalityText[i].innerHTML = personalityDescriptors[i]
+        }
+        else{
+            personalityText[i].innerHTML = personalityDescriptors[i+1]
+        }
     }
 }
 
@@ -195,5 +156,7 @@ restartButton.addEventListener('click', restartTest)
 function restartTest (){
     resultArea.classList.add('hide')
     startButton.classList.remove('hide')
-    counter -= 10;
+    counter -= 9;
+    userValues.length = 0
+    userTotals.length = 0
 }
