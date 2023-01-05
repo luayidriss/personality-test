6890c27addc132eb3821c336c5fe28a4d2c55f4f
luayidriss <idrissluay@gmail.com>
2023-01-05 13:45:46 +0000

    Add Previous Buttion function and Fix Calculate Funtionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,11 +23,19 @@ let answerValues =[
     prompt9= [1,2,3,4,5],
     prompt10= [1,2,3,4,5]
 ]
-    
-        
+
+let extroversion= 0;
+let agreeableness = 0;
+let conscientiousness = 0;
+let neuroticism = 0;
+let openness = 0
+
+
+     
 // Variables
 const startButton = document.getElementById("start-btn")
 const nextButton = document.getElementById("next-btn")
+const prevButton = document.getElementById("prev-btn")
 const testArea = document.getElementById("test-area")
 const moveButton = document.getElementsByClassName("move-btn")
 const promptElement =document.getElementById("prompts")
@@ -58,27 +66,68 @@ function runGame(){
     generateQuestions()
     }
     
-
-
 // Next Buttion Functionality
 nextButton.addEventListener('click', loadNextQuestion)
 function loadNextQuestion(){
-    counter++;
-    if (counter !== 10){
-        generateQuestions()
+        readScore()
+        counter++;
+        if (counter < 10){
+            generateQuestions()
+        }
+        else {
+            nextButton.innerText = `Finish`;
+            calculateScores();
+        } 
+    }   
+
+prevButton.addEventListener('click', loadPreviousQuestion)
+function loadPreviousQuestion(){
+    counter--;
+    removeScore()
+    if (counter < 0){
+        alert('This is the first question')
     }
     else {
-        alert("no more questions")
-        showResult()
+        generateQuestions()
+    } 
     }
+
+function readScore(){
+    const selectedOption = document.querySelector('input[type="radio"]:checked');
+    if(!selectedOption) {
+        alert('Please select your answer!');
+        return;
+    }
+    else{
+        var answerScore = parseInt(selectedOption.nextElementSibling.getAttribute('data-value'));
+        if(counter == 0 || counter == 5){
+            extroversion += answerScore;}
+        else if(counter == 1 || counter ==  6){
+            agreeableness += answerScore;}
+        else if(counter == 2 || counter ==  7){
+            conscientiousness += answerScore;}
+        else if(counter == 3 || counter ==  8){
+            neuroticism += answerScore;}
+        else if(counter == 4 || counter ==  9){
+            openness += answerScore;}
+        else{
+        }
+}}
+
+function removeScore(){
+    const selectedOption = document.querySelector('input[type="radio"]:checked');
+    var answerScore = parseInt(selectedOption.nextElementSibling.getAttribute('data-value'));
+        if(counter == 0 || counter == 5){
+            extroversion -= answerScore;}
+        else if(counter == 1 || counter ==  6){
+            agreeableness -= answerScore;}
+        else if(counter == 2 || counter ==  7){
+            conscientiousness -= answerScore;}
+        else if(counter == 3 || counter ==  8){
+            neuroticism -= answerScore;}
+        else if(counter == 4 || counter ==  9){
+            openness -= answerScore;}
+        else{
+            alert("Invalid Entry");
+        }
 }
-
-//Previous Button Functionality
-function loadPreviousQuestion(){
-
-}
-
-function showResult(){
-
-}
-
